af86252bb0ab08cbffa399c4ef75f5fdd3cb0762
JaiDevera <116239422+JaiDevera@users.noreply.github.com>
2022-10-27 15:29:00 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FrmBasicThread1
+{
+    public partial class Form1 : Form
+    {
+
+        private Thread Thread_1, Thread_2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart delthreadStart = new ThreadStart(MythreadClass.threads);
+
+            Thread_1 = new Thread(delthreadStart);
+            Thread_1.Name = "Thread A";
+            Thread_2 = new Thread(delthreadStart);
+            Thread_2.Name = "Thread B";
+
+            Thread_1.Start();
+            Thread_2.Start();
+
+            Thread_1.Join();
+            Thread_2.Join();
+
+            Console.WriteLine("-End of Thread-");
+            label1.Text = ("-End of Thread-");
+
+        }
+    }
+}
---
+++ MythreadClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace FrmBasicThread1
+{
+    class MythreadClass
+    {
+        public static void threads()
+        {
+            for (int loopcount = 0; loopcount  < 6; loopcount++)
+            {
+                Thread.Sleep(1500);
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process= " + loopcount);
+            }
+        }
+    }
+}
